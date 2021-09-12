f16e6e28e9cb8e4f068b0a6978deb2412d86bca5
Kacper Marczewski <kacper.marczewski@gmail.com>
2021-09-12 13:36:41 +0200

    pi estimated with monte carlo method

--- bj.cpp
+++ bj.cpp
@@ -5,9 +5,27 @@
 //
 //
 
-#include<stdio.h>
+#include <cstdio>
+#include <cmath>
+#include <random>
+
+const long COUNT = 1000000;
 
 int main(int argc, char *argv[]) {
-    printf("ok\n");
+    printf("trying... %ld\n", COUNT);
+    long inside = 0;
+    double x, y;
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<double> r(-1.0, 1.0);
+    for (int i = 0; i < COUNT; i++) {
+        x = r(mt);
+        y = r(mt);
+        if (sqrt(x * x + y * y) < 1.0)
+            inside++;
+    }
+    double pi = 4.0 * (double(inside)) / (double(COUNT));
+    printf("pi estimated = %.6f\n", pi);
+    printf("error = %.6f\n", abs(pi - M_PI));
     return 0;
 }
