c57edb0c5e3c645a513098743d32461e252ddb16
Kacper Marczewski <kacper.marczewski@gmail.com>
2021-10-09 23:13:01 +0200

    roman to arabic implementation start

--- roman.cpp
+++ roman.cpp
@@ -6,14 +6,32 @@
 //
 
 #include<stdio.h>
+#include <cstring>
 
-int roman_to_arabic(const char *roman);
+const int LEN = 7;
+const int CODE[] = {73, 86, 88, 76, 67, 68, 77};
+const int VAL[] = {1, 5, 10, 50, 100, 500, 1000};
+
+int roman_to_arabic(const char *r);
+
+int get_val(char c);
 
 int main(int argc, char *argv[]) {
-    printf("ok\n");
+    roman_to_arabic("IVXLCDM");
     return 0;
 }
 
-int roman_to_arabic(const char *roman) {
+int roman_to_arabic(const char *r) {
+    int len = strlen(r);
+    for (int i = 0; i < len; i++) {
+        printf("%c %d\n", r[i], get_val(r[i]));
+    }
     return 0;
+}
+
+int get_val(char c) {
+    for (int i = 0; i < LEN; i++) {
+        if (CODE[i] == c)
+            return VAL[i];
+    }
 }
