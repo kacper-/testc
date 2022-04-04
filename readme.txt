873bebd57509cc1fbaa96b80c93c15816823d720
Kacper Marczewski <kacper.marczewski@gmail.com>
2022-04-05 00:44:42 +0200

    tasker added

--- readme.txt
+++ readme.txt
@@ -1,3 +1,8 @@
+zaimplementować tasker - uruchamianie poleceń command line
+
+
+------------------------
 clang --target=aarch64-unknown-linux-gnu -stdlib=stdlibc++ -c pi.cpp
 
 Pobrać stdlib++ z ubuntu vm I skompilować do ELF
+
---
+++ tasker.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Kacper Marczewski on 05/04/2022
+//
+
+#include<string>
+#include<iostream>
+#include<thread>
+#include<vector>
+
+void print(int n, const std::string &str)  {
+    std::cout << n << " " << str << std::endl;
+}
+
+int main() {
+    std::vector<std::string> s = {
+            "Educative.blog",
+            "Educative",
+            "courses",
+            "are great"
+    };
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < s.size(); i++) {
+        threads.push_back(std::thread(print, i, s[i]));
+    }
+
+    for (auto &th : threads) {
+        th.join();
+    }
+    return 0;
+}
