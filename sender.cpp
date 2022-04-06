c2543a5e59f1e32240b56c0dbffa09a9e3250d88
Kacper Marczewski <kmarczewski@asset-control.com>
2022-04-06 11:37:53 +0200

    sender final form

--- sender.cpp
+++ sender.cpp
@@ -23,26 +23,32 @@ void errno_abort(const char* header)
 
 int main(int argc, char* argv[])
 {
-    struct sockaddr_in send_addr, recv_addr;
-    int trueflag = 1, count = 0;
+    struct sockaddr_in send_addr;
+    int trueflag = 1;
     int fd;
+
+    if(argc != 4) {
+        printf("Usage: sender ip_address port message\n");
+        printf("submitted parameters [%d]:\n", argc);
+        for(int i=0;i<argc;i++)
+            printf("\t%s\n", argv[i]);
+        return 0;
+    }
+
     if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
         errno_abort("socket");
-    if (setsockopt(fd, SOL_SOCKET, SO_BROADCAST,
-                   &trueflag, sizeof trueflag) < 0)
+    if (setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &trueflag, sizeof trueflag) < 0)
         errno_abort("setsockopt");
 
     memset(&send_addr, 0, sizeof send_addr);
     send_addr.sin_family = AF_INET;
-    send_addr.sin_port = (in_port_t) htons(4455);
-    // broadcasting address for unix (?)
-    inet_aton("192.168.1.255", &send_addr.sin_addr);
-        char sbuf[256] = {};
-        snprintf(sbuf, sizeof(sbuf), "Hello(%d)!", count++);
-        if (sendto(fd, sbuf, strlen(sbuf)+1, 0,
-                   (struct sockaddr*) &send_addr, sizeof send_addr) < 0)
-            errno_abort("send");
-        printf("send: %s\n", sbuf);
-        usleep(1000000/2);
+    send_addr.sin_port = (in_port_t) htons(atoi(argv[2]));
+    inet_aton(argv[1], &send_addr.sin_addr);
+
+    if (sendto(fd, argv[3], strlen(argv[3]), 0, (struct sockaddr*) &send_addr, sizeof send_addr) < 0)
+        errno_abort("sendto");
+
+    printf("sent: %s\n", argv[3]);    
+
     return 0;
 }
