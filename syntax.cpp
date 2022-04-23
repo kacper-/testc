f71df3bf3e72e6d3ffb77364f447bf4c6c63fef6
Kacper Marczewski <kacper.marczewski@gmail.com>
2022-04-23 16:38:55 +0200

    references

--- syntax.cpp
+++ syntax.cpp
@@ -5,6 +5,15 @@
 
 int main(int argc, char *argv[]) {
     int a;
+    int b=4;
     printf("%d\n", a=3);
+
+    int& aref = a;
+    printf("%d\n", aref);
+    aref = b;
+    printf("%d\n", aref);
+
+    std::cout << &aref << std::endl << &a;
+    std::cout << aref << std::endl;
     return 0;
 }
