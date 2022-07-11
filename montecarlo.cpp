499e8cd079ceda3822386be259b8078bdb286494
Kacper Marczewski <kacper.marczewski@gmail.com>
2022-07-12 00:16:26 +0200

    monte carlo pi

---
+++ montecarlo.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Kacper Marczewski on 09/10/2021.
+//
+// Conversion between roman to arabic and arabic to roman numerals
+//
+//
+
+#include <cstdio>
+#include <cstring>
+#include <random>
+#include <unistd.h>
+
+std::random_device rd;
+std::mt19937 mt(rd());
+std::uniform_int_distribution<float> get_float(-1, 1);
+
+int main(int argc, char *argv[]) {
+    printf("Monte Carlo PI\n");
+    
+}
