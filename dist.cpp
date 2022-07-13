107fcc20f4c96811b525a3e92e00ae01982a80bb
Kacper Marczewski <kacper.marczewski@gmail.com>
2022-07-13 20:46:53 +0200

    better distribution

--- dist.cpp
+++ dist.cpp
@@ -14,16 +14,16 @@ std::random_device rd;
 std::mt19937 mt(rd());
 std::uniform_real_distribution<float> get_float(0, 1);
 
-const int ITER = 10000000;
+const int ITER = 100000000;
 
 int main(int argc, char *argv[]) {
-    int t[5];
-    for(int i=0;i<5;i++)
+    int t[10];
+    for(int i=0;i<10;i++)
         t[i]=0;
     for(int i=0;i<ITER;i++)
-        t[(int)(5 * get_float(mt))]++;
-    for(int i=0;i<5;i++) {
-        int diff = abs((ITER/5) - t[i]);
-        printf("%d = %d diff = %f\n", i + 1, t[i], ((float)diff) / ((float) ITER/5));
+        t[(int)(10 * get_float(mt))]++;
+    for(int i=0;i<10;i++) {
+        int diff = abs((ITER/10) - t[i]);
+        printf("%d = %d \tdiff = %f\n", i + 1, t[i], ((float)diff) / ((float) ITER/10));
     }
 }
