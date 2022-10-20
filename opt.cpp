b129bf88b6f7481696cde9d6ba46fc59f5dbf255
Kacper Marczewski <kacper.marczewski@gmail.com>
2022-10-20 14:52:58 +0200

    new files added

---
+++ opt.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[]) {
+
+int a=9;
+int b=4;
+int c=7;
+int d=1;
+int e=3;
+
+while((a=1)==1 && (b=2)==3 && (c=3)==3 || (d=4)==4 || (e=5)==7) {
+    printf("a=%d b=%d c=%d d=%d e=%d\n", a, b, c, d, e);
+    break;
+}
+
+}
