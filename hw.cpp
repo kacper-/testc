0aa37eff4b2ebecb7f5f146b7222b23556c0df01
Kacper Marczewski <kacper.marczewski@gmail.com>
2022-11-20 17:50:30 +0100

    new executable

---
+++ hw.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+
+int main() {
+    std::cout << "Content:type: text/html" << std::endl << std::endl;
+    std::cout << "<html><body><h1>Hello</h1>" << std::endl;
+    std::cout << "</body></html>" << std::endl;
+    return 0;
+}
